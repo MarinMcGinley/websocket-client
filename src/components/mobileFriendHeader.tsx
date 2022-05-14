334e3db20e45cd779c1910bbf6095deea0634954
Marin McGinley <marinmcginley@Marins-MacBook-Pro.local>
2022-05-14 17:58:50 +0000

    mobile-view for friends finished

---
+++ src/components/mobileFriendHeader.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import { useNavigate } from "react-router-dom";
+import "./mobileFriendHeader.scss";
+
+type FriendProp = {
+  firstName: string;
+  lastName: string;
+};
+
+const MobileFriendHeader = (props: FriendProp) => {
+  const navigate = useNavigate();
+
+  const goBack = (event: any) => {
+    event.preventDefault();
+    console.log("goback");
+    navigate("/");
+  };
+  return (
+    <div className="mobile-friend-header-container">
+      <button className="arrow-back-button" onClick={(e) => goBack(e)}>
+        <i className="fa-solid fa-arrow-left"></i>
+      </button>
+      <h3>{props.firstName}</h3>
+    </div>
+  );
+};
+
+export default MobileFriendHeader;
--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,8 +1,9 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 import * as React from "react";
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { MouseEventHandler, useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import Header from "../components/header";
+import MobileMessageChat from "./mobileMessageChat";
 import "./homePage.scss";
 
 const serverUrl = process.env.REACT_APP_WEBSOCKET_URL;
@@ -16,89 +17,100 @@ type Friend = {
 
 const HomePage = () => {
   const navigate = useNavigate();
-  const [friends, setFriends] = useState([]);
+
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 800);
+  const [friends, setFriends] = useState([
+    {
+      id: 1,
+      email: "annagyda@gmail.com",
+      firstName: "Anna Gyða",
+      lastName: "Gunnlaugsdóttir",
+    },
+    {
+      id: 2,
+      email: "annagyda@gmail.com",
+      firstName: "Marín Ingibjörg",
+      lastName: "McGinley",
+    },
+    {
+      id: 3,
+      email: "annagyda@gmail.com",
+      firstName: "Jóhanna",
+      lastName: "Gunnlaugsdóttir",
+    },
+    {
+      id: 4,
+      email: "annagyda@gmail.com",
+      firstName: "Páll",
+      lastName: "Gunnlaugsson",
+    },
+    {
+      id: 5,
+      email: "annagyda@gmail.com",
+      firstName: "Iðunn",
+      lastName: "Pálsdóttir",
+    },
+    {
+      id: 6,
+      email: "annagyda@gmail.com",
+      firstName: "Edda",
+      lastName: "Pálsdóttir",
+    },
+    {
+      id: 7,
+      email: "annagyda@gmail.com",
+      firstName: "Logi",
+      lastName: "Pálsson",
+    },
+    {
+      id: 8,
+      email: "annagyda@gmail.com",
+      firstName: "Inga Hlíf",
+      lastName: "Melvinsdóttir",
+    },
+    {
+      id: 9,
+      email: "annagyda@gmail.com",
+      firstName: "Eirik",
+      lastName: "Brandsas",
+    },
+    {
+      id: 10,
+      email: "annagyda@gmail.com",
+      firstName: "Pétur Sólmar",
+      lastName: "Guðjónsson",
+    },
+    {
+      id: 11,
+      email: "annagyda@gmail.com",
+      firstName: "Erna Þórey",
+      lastName: "Jónasdóttir",
+    },
+    {
+      id: 12,
+      email: "annagyda@gmail.com",
+      firstName: "Jóhanna Margrét",
+      lastName: "Logadóttir",
+    },
+    {
+      id: 13,
+      email: "annagyda@gmail.com",
+      firstName: "Edda Katrín",
+      lastName: "Logadóttir",
+    },
+  ]);
+  const [chosenFriend, setChosenFriend] = useState(null);
 
   useEffect(() => {
-    setFriends([
-      {
-        id: 1,
-        email: "annagyda@gmail.com",
-        firstName: "Anna Gyða",
-        lastName: "Gunnlaugsdóttir",
-      },
-      {
-        id: 2,
-        email: "annagyda@gmail.com",
-        firstName: "Marín Ingibjörg",
-        lastName: "McGinley",
-      },
-      {
-        id: 3,
-        email: "annagyda@gmail.com",
-        firstName: "Jóhanna",
-        lastName: "Gunnlaugsdóttir",
-      },
-      {
-        id: 4,
-        email: "annagyda@gmail.com",
-        firstName: "Páll",
-        lastName: "Gunnlaugsson",
-      },
-      {
-        id: 5,
-        email: "annagyda@gmail.com",
-        firstName: "Iðunn",
-        lastName: "Pálsdóttir",
-      },
-      {
-        id: 6,
-        email: "annagyda@gmail.com",
-        firstName: "Edda",
-        lastName: "Pálsdóttir",
-      },
-      {
-        id: 7,
-        email: "annagyda@gmail.com",
-        firstName: "Logi",
-        lastName: "Pálsson",
-      },
-      {
-        id: 8,
-        email: "annagyda@gmail.com",
-        firstName: "Inga Hlíf",
-        lastName: "Melvinsdóttir",
-      },
-      {
-        id: 9,
-        email: "annagyda@gmail.com",
-        firstName: "Eirik",
-        lastName: "Brandsas",
-      },
-      {
-        id: 10,
-        email: "annagyda@gmail.com",
-        firstName: "Pétur Sólmar",
-        lastName: "Guðjónsson",
-      },
-      {
-        id: 11,
-        email: "annagyda@gmail.com",
-        firstName: "Erna Þórey",
-        lastName: "Jónasdóttir",
-      },
-      {
-        id: 12,
-        email: "annagyda@gmail.com",
-        firstName: "Jóhanna Margrét",
-        lastName: "Logadóttir",
-      },
-      {
-        id: 13,
-        email: "annagyda@gmail.com",
-        firstName: "Edda Katrín",
-        lastName: "Logadóttir",
-      },
-    ]);
+    window.addEventListener("resize", () =>
+      setIsMobile(window.innerWidth < 800)
+    );
+    return () => {
+      window.removeEventListener("resize", () =>
+        setIsMobile(window.innerWidth < 800)
+      );
+    };
+
     // axios
     //   .get(`${serverUrl}/api/friends`, {
     //     headers: {
@@ -115,20 +127,45 @@ const HomePage = () => {
     //   });
   });
 
-  const friendsToDisplay = friends.map((friend: Friend) => {
+  const mobileView = () => {
     return (
-      <ul className="friends-container">
-        <li key={friend.id} className="friend">
-          {friend.firstName} {friend.lastName}
-        </li>
-      </ul>
+      <div className="friends-container">
+        {friends.map((friend: Friend) => {
+          return (
+            <button
+              key={friend.id}
+              className="friend-button"
+              onClick={(e: any) => {
+                navigate(
+                  `friend/${friend.id}/${friend.firstName}/${friend.lastName}`
+                );
+              }}
+            >
+              {friend.firstName} {friend.lastName}
+            </button>
+          );
+        })}
+        ;
+      </div>
     );
-  });
+  };
 
+  const fullScreenView = () => {};
+
+  // return (
+  //   <React.Fragment>
+  //     <Header />
+  //     {chosenFriend == null ? (
+  //       <ul className="friends-container">{friendsToDisplay}</ul>
+  //     ) : (
+  //       <MobileMessageChat friendId={chosenFriend} />
+  //     )}
+  //   </React.Fragment>
+  // );
   return (
     <React.Fragment>
       <Header />
-      {friendsToDisplay}
+      {isMobile ? mobileView() : "NOT MOBILE"}
     </React.Fragment>
   );
 };
